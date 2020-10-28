d3398ef605419901d9e929ddc5cf048f378f9e40
Lia Sexton <liamaggiorotto@gmail.com>
2020-10-27 21:09:10 -0500

    adds base file structure and styles initil index page

---
+++ public/scripts/app.js
@@ -0,0 +1,1 @@
+console.log("it's Britney b*itch");
---
+++ server.js
@@ -0,0 +1,41 @@
+// --------------------- External Modules
+const express = require('express');
+const path = require('path');
+const methodOverride = require('method-override');
+
+
+// --------------------- Internal Modules
+// const controllers = require("./controllers");
+
+
+// --------------------- Instanced Modules
+const app = express();
+
+
+// --------------------- Configuration
+const PORT = 3000;
+app.set("view engine", "ejs");
+
+
+// --------------------- Middleware
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.originalUrl}`);
+    next();
+});
+
+
+// --------------------- Routes
+
+// view, home page
+app.get("/", function (req, res)  {
+    res.render("index", {});
+});
+
+
+// --------------------- Server Listener
+app.listen(PORT, () => {
+    console.log(`Listening for client request on ${PORT}`)
+});
