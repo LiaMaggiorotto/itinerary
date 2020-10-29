d0806b3c21dab38b93e8c4fb8f80c6b6f777cd20
Lia Sexton <liamaggiorotto@gmail.com>
2020-10-29 15:11:30 -0500

    adds edit and update routes to trip and edit.ejs view

--- controllers/trips.js
+++ controllers/trips.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const db = require('../models');
 
-const categories = ['Arriving Flights, Departing Flights, Itinerary, Notes'];
+const categories = ['Itinerary', 'Arriving Flights', 'Departing Flights', 'Notes'];
 
 // base route is /trips
 
@@ -45,13 +45,47 @@ router.post('/', function (req, res) {
 })
 
 // --------------------- Show
+router.get('/:id', function(req,res) {
+    db.Trip.findById(req.params.id, (err, oneTripFromDB) => {
+        if(err){
+            console.log(err);
+        } else {
+            const context = {
+                categories: categories,
+                oneTrip: oneTripFromDB
+            }
+            res.render('trip/show', context)
+        }
+    })
+})
 
 
 // --------------------- Edit
+router.get('/:id/edit', function (req,res) {
+    db.Trip.findById(req.params.id, (err, oneTripFromDB) => {
+        if (err) {
+            console.log(err);
+        } else {
+            const context = {
+                oneTrip: oneTripFromDB
+            }
+            res.render('trip/edit', context)
+        }
+    })
+})
 
 
 // --------------------- Update
-
+router.put('/:id', function (req, res) {
+    db.Trip.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true }, (err, updatedTrip) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(updatedTrip);
+            res.redirect(`/trips/${req.params.id}`)
+        }
+    })
+})
 
 // --------------------- Delete
 
